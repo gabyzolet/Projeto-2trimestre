5956b4bfec58a2145b31deeeb3f59c350f1352ad
gabyzolet <108010808+gabyzolet@users.noreply.github.com>
2024-08-05 08:53:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,6 +40,25 @@ const perguntas = [
             "Alternativa 2"
         ]
     },
-    
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
 
